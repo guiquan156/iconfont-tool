4c87e66d86f07a60df7828024cdeead1873e8bd2
lian <guiquan156@163.com>
2019-03-09 15:12:29 +0800

    update

--- main.js
+++ main.js
@@ -1,14 +1,15 @@
-import { app, BrowserWindow } from 'electron';
+var { app, BrowserWindow } = require('electron');
 
 function createWindow () {
-    const win = new BrowserWindow({ width: 800, height: 600 });
-    win.webContents.openDevTools();
+  const win = new BrowserWindow({ width: 800, height: 600 });
+  win.webContents.openDevTools();
 
-    win.loadFile();
+  // win.loadFile(`file://${__dirname}/web/dist/index.html`);
+  win.loadURL('http://localhost:8000');
 
-    win.on('closed', () => {
-        win = null
-    });
+  win.on('closed', () => {
+    win = null
+  });
 }
 
 app.on('ready', createWindow);
--- web/src/layouts/index.js
+++ web/src/layouts/index.js
@@ -2,10 +2,7 @@ import styles from './index.css';
 
 function BasicLayout(props) {
   return (
-    <div className={styles.normal}>
-      <h1 className={styles.title}>Yay! Welcome to umi!</h1>
-      { props.children }
-    </div>
+    <div></div>
   );
 }
 
