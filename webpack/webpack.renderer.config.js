f62092aff33eb78a674195879d5a78bd35b61d8e
lian <guiquan156@163.com>
2019-03-10 22:00:59 +0800

    update

--- webpack/webpack.renderer.config.js
+++ webpack/webpack.renderer.config.js
@@ -1,5 +1,47 @@
+const path = require('path');
 const webpack = require('webpack');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+const extractSass = new ExtractTextPlugin({
+    filename: "[name].[contenthash].css",
+    disable: process.env.NODE_ENV === "development"
+});
 
 module.exports = {
-
+    entry: {
+        app: './src/renderer/app.js'
+    },
+    output: {
+        path: path.resolve(__dirname, "app/renderer"),
+        filename: '[name].[hash].js'
+    },
+    target: 'electron-renderer',
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        preset: ['es2015', 'react']
+                    }
+                },
+                exclude: /node_modules/
+            },
+            {
+                test: /\.(scss|sass)$/,
+                use: extractSass.extract({
+                    use: [{
+                        loader: 'style-loader'
+                    }, {
+                        loader: 'css-loader'
+                    }],
+                    fallback: 'style-loader'
+                })
+            }
+        ]
+    },
+    plugins: [
+        extractSass
+    ]
 };
