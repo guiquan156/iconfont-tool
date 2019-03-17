8d30a04cb9f534d7a7276de4b9f10616a551c98c
lian <guiquan156@163.com>
2019-03-17 20:22:55 +0800

    append files

--- src/renderer/pages/Home/index.jsx
+++ src/renderer/pages/Home/index.jsx
@@ -1,28 +1,64 @@
 import React from 'react';
-import { Button } from 'antd';
-import { ipcRenderer } from 'electron';
+import { Button, Empty, List } from 'antd';
 import * as ipc from 'services/ipc';
 
 import './index.scss';
 
 export default class Home extends React.Component {
 
+    state = {
+        fileList: []
+    }
+
     selectFile = () => {
         ipc.send('selectSvgFile', {
             success: (e, { data }) => {
-                const files = data.files;
-
-                console.log(files)
+                const files = data.files.filter((file) => {
+                    return this.state.fileList.indexOf(file) === -1;
+                });
+                this.setState({
+                    fileList: files.concat(this.state.fileList)
+                });
             }
         });
     }
 
     render () {
+        const { fileList } = this.state;
         return (
             <div className="page-home">
                 <h2>iconfont 生成工具</h2>
-                <Button type="primary" onClick={this.selectFile}>选择文件</Button>
+                { fileList.length > 0
+                    ? <SelectedList fileList={fileList} addFiles={this.selectFile} />
+                    : <FileEmpty selectFile={this.selectFile} /> }
             </div>
         )
     }
+}
+
+
+function FileEmpty (props) {
+    return (
+        <Empty description="请选择svg文件">
+            <Button type="primary" onClick={props.selectFile}>选择文件</Button>
+        </Empty>
+    );
+}
+
+function SelectedList ({ fileList, addFiles }) {
+    return (
+        <div>
+            <List
+                size="small"
+                bordered
+                dataSource={fileList}
+                renderItem={item => (<List.Item>{item}</List.Item>)}
+            />
+
+            <div className="footer">
+                <Button className="add" onClick={addFiles}>继续添加</Button>
+                <Button type="primary">生成iconfont</Button>
+            </div>
+        </div>
+    );
 }
